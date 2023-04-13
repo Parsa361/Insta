d7fe84bc4f5e36727fface230adeca379fd2205a
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-13 22:08:52 +0330

    Project created

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+
+</script>
+
+<template>
+  <main>
+    <Navbar />
+  </main>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
