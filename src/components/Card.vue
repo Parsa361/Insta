bacd4267dcd5b8efc9741eec3a06e7427999aecf
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-19 17:37:23 +0330

    Add post card component

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+const props = defineProps(['post'])
+</script>
+
+<template>
+    <ACard hoverable style="width: 240px" class="card">
+        <template #cover>
+            <img :alt="post.title" :src="post.url" />
+        </template>
+        <ACardMeta :title="post.title">
+            <template #description>{{post.caption }}</template>
+        </ACardMeta>
+    </ACard>
+</template>
+
+<style scoped>
+.card {
+    margin: 20px 0;
+}
+</style>
