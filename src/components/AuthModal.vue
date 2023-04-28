8fbd8a2c9ba1c35d849f7c6ccb18640d8c7a3fa5
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-28 22:23:38 +0330

    Finished the sign up logic

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,19 +1,45 @@
-
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
+import { useUserStore } from '../stores/users'
+import { storeToRefs } from 'pinia'
 
-const props = defineProps(['isLogin'])
+const userStore = useUserStore();
+
+const { errorMsg, loading, user } = storeToRefs(userStore);
+
+const props = defineProps(['isLogin']);
 const visible = ref(false);
+
+const userCredentials = reactive({
+    email: '',
+    password: '',
+    username: ''
+});
 
 const showModal = () => {
     visible.value = true;
 };
 
-const handleOk = (e) => {
+const handleOk = async (e) => {
+    await userStore.handleSignup(userCredentials)
+    if (user.value) {
+        clearUserCredential();
+        visible.value = false;
+    }
+};
+
+const handleCancel = () => {
+    clearUserCredential();
     visible.value = false;
 };
 
-const title = props.isLogin ? 'Login' : 'Signup'
+const clearUserCredential = () => {
+    userCredentials.email = '';
+    userCredentials.password = '';
+    userCredentials.username = '';
+    userStore.clearErrorMsg();
+}
+const title = props.isLogin ? 'Login' : 'Signup';
 
 </script>
 
@@ -22,9 +48,19 @@ const title = props.isLogin ? 'Login' : 'Signup'
     <div>
         <AButton type="primary" @click="showModal">{{ title }}</AButton>
         <AModal v-model:visible="visible" :title="title" @ok="handleOk">
-            <AInput class="input" v-if="!isLogin" v-model:value="value" placeholder="Username" />
-            <AInput class="input" v-model:value="value" placeholder="Email" />
-            <AInput class="input" v-model:value="value" placeholder="Password" />
+            <template #footer>
+                <AButton key="back" @click="handleCancel">Cancel</AButton>
+                <AButton :disabled="loading" key="submit" type="primary" @click="handleOk">Submit</AButton>
+            </template>
+            <div v-if="!loading" class="input-container">
+                <AInput class="input" v-if="!isLogin" v-model:value="userCredentials.username" placeholder="Username" />
+                <AInput class="input" v-model:value="userCredentials.email" placeholder="Email" />
+                <AInput class="input" v-model:value="userCredentials.password" placeholder="Password" type="password" />
+            </div>
+            <div v-else class="spinner">
+                <ASpin />
+            </div>
+            <a-typography-text type="danger">{{ errorMsg }}</a-typography-text>
         </AModal>
     </div>
 </template>
@@ -32,5 +68,16 @@ const title = props.isLogin ? 'Login' : 'Signup'
 <style scoped>
 .input {
     margin-top: 5px;
+}
+
+.input-container {
+    height: 120px;
+}
+
+.spinner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
 }
 </style>
