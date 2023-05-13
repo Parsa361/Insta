66e88b23a7d87f24ae0ed87e74bd56c0a9134c06
Parsa Farahani <parsafarahany821@gmail.com>
2023-05-13 22:42:38 +0330

    Added posts to the user profile here

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const props = defineProps(['posts'])
+</script>
+
+<template>
+    <div class="image-gallery-container">
+        <img v-for="post in props.posts" :key="post.id"
+            :src='`https://zppnutenrqrbzxjnckpd.supabase.co/storage/v1/object/public/images/${post.url}`'>
+    </div>
+</template>
+
+<style scoped>
+.image-gallery-container {
+    display: flex;
+    justify-content: left;
+    flex-wrap: wrap;
+}
+
+img {
+    margin-right: 20px;
+    border-radius: 5px;
+    width: 250px;
+    object-fit: cover;
+}
+</style>
